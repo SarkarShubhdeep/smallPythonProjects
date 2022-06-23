0dc45afd72e17fad27a959335e7e82cb00cc530b
Shubhdeep Sarkar <sarkarshubhdeep1@gmail.com>
2022-06-23 19:18:17 +0530

    Madlib... just a normal, regular, madlib.

---
+++ madlibs.py
@@ -0,0 +1,81 @@
+
+# (starring s, s,and s)
+# NURSE: Thank goodness you're here, doctor. A patient was just
+# brought in with a badly bruised s and a ruptured
+# S. Unfortunately, Dr. Smith plans to operate s.
+# DOCTOR: We can't let him! Look at the way his s is shaking.
+# NURSE: Uh, oh, he's putting a mask over his s! Doctor,
+# stop him before he s somebody.
+# DOCTOR: Smith, you can't operate on this s! forbid it.
+# SMITH: How dare you say that to me. I'm your mentor. You're like
+# a s to me.
+#
+# DOCTOR: And you're like as to me, but I can't risk the
+# wrath of as to satisfy your ego. Look in the mirror.
+# Would you trust that s to remove a S nail?
+
+
+# Adjective:
+#
+# Adverb:
+#
+# Noun #1:
+#
+# Noun #2:
+#
+# Noun #3:
+#
+# Noun #4:
+#
+# Noun #5:
+#
+# Noun #6:
+#
+# Part of the Body #1:
+#
+# Part of the Body #2:
+# Person's Name #1:
+#
+# Person's Name #2:
+#
+# Person's Name #3:
+#
+# Verb Ending in S:
+
+print("👋 Welcome to Madlibs")
+print("Enter your word choices ")
+adj = input("Adjective: ")
+adverb = input("Adverb: ")
+n1 = input("Noun: ")
+n2 = input("Noun: ")
+n3 = input("Noun: ")
+n4 = input("Noun: ")
+n5 = input("Noun: ")
+n6 = input("Noun: ")
+bodyPart1 = input("Part of the body: ")
+bodyPart2 = input("Part of the body: ")
+name1 = input("Person's name: ")
+name2 = input("Person's name: ")
+name3 = input("Person's name: ")
+verbS = input("Verb ending in S: ")
+
+print("\n\n")
+print("Medical Drama".upper())
+
+print(f"(starring {name1}, {name2},and {name3})\n\n")
+print(
+    f"NURSE: Thank goodness you're here, doctor. A patient was just\n"
+    f"brought in with a badly bruised {bodyPart1} and a ruptured\n"
+    f"{n1}. Unfortunately, Dr. Smith plans to operate {adverb}.\n"
+    f"DOCTOR: We can't let him! Look at the way his {bodyPart2} is shaking.\n"
+    f"NURSE: Uh, oh, he's putting a mask over his {n2}! Doctor,\n"
+    f"stop him before he {verbS} somebody.\n"
+    f"DOCTOR: Smith, you can't operate on this {n3}! forbid it.\n"
+    "SMITH: How dare you say that to me. I'm your mentor. You're like\n"
+    f"a {n4} to me.\n"
+    f"DOCTOR: And you're like a {n5} to me, but I can't risk the\n"
+    f"wrath of a {n6} to satisfy your ego. Look in the mirror.\n"
+    f"Would you trust that {n6} to remove a {adj} nail?\n"
+)
+
+
